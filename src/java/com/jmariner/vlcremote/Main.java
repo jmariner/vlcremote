685403d06a453d879aefd8d069e5846f8c88c3b2
Justin <justin.mariner@hotmail.com>
2016-10-19 15:41:22 -0400

    cleanup

--- src/java/com/jmariner/vlcremote/Main.java
+++ src/java/com/jmariner/vlcremote/Main.java
@@ -7,12 +7,8 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
-		//*
-		GlobalHotkeyListener g = new GlobalHotkeyListener();
 
-		g.registerHotkey("meta y", () -> {
-			log.info("worked!");
-		});//*/
+
 	}
 
 }
