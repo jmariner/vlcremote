ba08912a72aed96e8ea2b24debafda50e73d5270
Justin <justin.mariner@hotmail.com>
2016-10-31 15:53:32 -0400

    Reverting attempt at saving playback info

--- src/java/com/jmariner/vlcremote/RemoteInterface.java
+++ src/java/com/jmariner/vlcremote/RemoteInterface.java
@@ -296,9 +296,7 @@ public class RemoteInterface extends JFrame {
 		volumeSlider.addChangeListener(this::volumeChanged);
 		volumeButton.addActionListener(this::toggleMute);
 		volumeTextField.addActionListener(this::volumeInputted);
-	}
 
-	private void initActionListenersPost() {
 		togglePlaylistButton.addActionListener(this::togglePlaylistArea);
 	}
 
@@ -335,13 +333,11 @@ public class RemoteInterface extends JFrame {
 			controls.forEach(b -> b.setEnabled(true));
 
 			playlistPanel = new PlaylistPanel(this);
-			initActionListenersPost();
 			initHotkeys();
 
 			remote.setSourceVolume(1);
 			remote.sendCommand(Command.PLAY);
-			updateInterface(remote.getStatus(), true);
-			loadPlaybackOptions();
+			updateInterface(remote.getStatus());
 			startUpdateLoop();
 			remote.playStream();
 		}
@@ -378,21 +374,6 @@ public class RemoteInterface extends JFrame {
 			UserSettings.putBoolean("autoconnect", autoconnect);
 		}
 	}
-	
-	private void loadPlaybackOptions() {
-		// after initial interface update, so button states are accurate to current options
-		
-		if (shuffleToggleButton.isSelected() != UserSettings.getBoolean("shuffle", false))
-			shuffleToggleButton.doClick();
-		if (loopToggleButton.isSelected() != UserSettings.getBoolean("loop", false))
-			loopToggleButton.doClick();
-		if (repeatToggleButton.isSelected() != UserSettings.getBoolean("repeat", false))
-			repeatToggleButton.doClick();
-		if (volumeButton.isSelected() != UserSettings.getBoolean("muted", false))
-			volumeButton.doClick();
-		
-		volumeSlider.setValue(UserSettings.getInt("volume", 100));
-	}
 
 	private void initRemote() {
 		char[] password = passwordField.getPassword();
@@ -409,12 +390,8 @@ public class RemoteInterface extends JFrame {
 	public void updateInterface() {
 		updateInterface(null);
 	}
-	
-	public void updateInterface(Map<String, String> status) {
-		updateInterface(status, false);
-	}
 
-	private void updateInterface(Map<String, String> status, boolean firstRun) {
+	public void updateInterface(Map<String, String> status) {
 
 		int volume = volumeSlider.getValue();
 		if (!volumeTextField.hasFocus())
@@ -475,16 +452,6 @@ public class RemoteInterface extends JFrame {
 		positionLabel.setText(GuiUtils.formatTime(currentTime));
 		progressBar.setValue((int) (positionPercent * progressBar.getMaximum()));
 		
-		if (!firstRun)
-			savePlaybackOptions();
-	}
-	
-	private void savePlaybackOptions() {
-		UserSettings.putBoolean("shuffle", shuffleToggleButton.isSelected());
-		UserSettings.putBoolean("repeat", repeatToggleButton.isSelected());
-		UserSettings.putBoolean("loop", loopToggleButton.isSelected());
-		UserSettings.putBoolean("muted", muted);
-		UserSettings.putInt("volume", volumeSlider.getValue());
 	}
 
 	private void startUpdateLoop() {
